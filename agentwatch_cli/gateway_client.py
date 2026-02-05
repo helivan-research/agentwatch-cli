6ad21a55ce1fd6e20907edb78f46e96c530312f2
hshelm <haydenshelm@gmail.com>
2026-02-05 06:46:33 +0000

    Add debug output for connect request

--- agentwatch_cli/gateway_client.py
+++ agentwatch_cli/gateway_client.py
@@ -79,6 +79,7 @@ class GatewayClient:
                 if self.token:
                     connect_req["params"]["auth"] = {"token": self.token}
 
+                print(f"[DEBUG] Sending connect request: {json.dumps(connect_req, indent=2)}")
                 await self._ws.send(json.dumps(connect_req))
 
                 # Wait for connect response directly
